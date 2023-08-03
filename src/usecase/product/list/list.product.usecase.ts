ad6fd0871f8517319d27c74bdf0dd448c6c0c13b
kelton99 <keltonfreitas53@gmail.com>
2023-08-03 12:02:56 -0300

    add list product use case

---
+++ src/usecase/product/list/list.product.usecase.ts
@@ -0,0 +1,32 @@
+import ProductInterface from "../../../domain/product/entity/product.interface";
+import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import { InputListProductDto, OutputListProductDto } from "./list.product.dto";
+
+export default class ListProductUseCase {
+
+  private productRepository: ProductRepositoryInterface;
+
+  constructor(productRepository: ProductRepository) {
+    this.productRepository = productRepository;
+  }
+
+  async execute(input: InputListProductDto): Promise<OutputListProductDto> {
+
+    const products = await this.productRepository.findAll();
+    return OutputMapper.toOutput(products);
+  }
+
+}
+
+class OutputMapper {
+  static toOutput(product: ProductInterface[]): OutputListProductDto {
+    return {
+      products: product.map((product) => ({
+        id: product.id,
+        name: product.name,
+        price: product.price
+      })),
+    };
+  }
+}
