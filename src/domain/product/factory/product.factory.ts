db64bd0a60e86d51cd3ef1ee98141eaa64191ae7
kelton99 <keltonfreitas53@gmail.com>
2023-08-03 11:25:28 -0300

    fix product factory

--- src/domain/product/factory/product.factory.ts
+++ src/domain/product/factory/product.factory.ts
@@ -1,11 +1,21 @@
 import Product from "../entity/product";
 import { v4 as uuid } from "uuid";
+import ProductB from "../entity/product-b";
+import ProductInterface from "../entity/product.interface";
 
 export default class ProductFactory {
   public static create(
+    type: string,
     name: string,
     price: number
-  ): Product {
-    return new Product(uuid(), name, price);
+  ): ProductInterface {
+    switch (type) {
+      case "a":
+        return new Product(uuid(), name, price);
+      case "b":
+        return new ProductB(uuid(), name, price);
+      default:
+        throw new Error("Product type not supported");
+    }
   }
 }
--- src/usecase/product/create/create.product.usecase.ts
+++ src/usecase/product/create/create.product.usecase.ts
@@ -13,7 +13,7 @@ export default class CreateProductUseCase {
 
   async execute(input: InputCreateProductDto): Promise<OutputCreateProductDto> {
 
-    const product = ProductFactory.create(input.name, input.price);
+    const product = ProductFactory.create('a', input.name, input.price);
 
     await this.productRepository.create(product);
     
